6e0225de4c7077ae2c4fc85d14a3af7aba640747
M0nt <67793307+montajebii@users.noreply.github.com>
2024-09-28 15:13:46 +0330

    test

---
+++ math/342/342.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        list = [1, 4, 6]
+        if n[-1] in list:
+            print("yes")
+        else:
+            print("no")
+while True:
+    Solution(input())
+        
